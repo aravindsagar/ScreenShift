680288b7e35a9e0d842477f99ac17abb08fd840a
Aravind Sagar <aravindsuhruth@gmail.com>
2015-06-19 14:28:11 +0530

    Tweaks in layouts and colors

--- app/src/main/java/com/sagar/screenshift/MainActivity.java
+++ app/src/main/java/com/sagar/screenshift/MainActivity.java
@@ -735,31 +735,6 @@ public class MainActivity extends AppCompatActivity implements DialogFragments.D
         switchView.setChecked(isChecked);
     }
 
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, ScreenShiftService.class);
-            intent.setAction(ScreenShiftService.ACTION_STOP);
-            startService(intent);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
     @Override
     protected void onPause() {
         super.onPause();
